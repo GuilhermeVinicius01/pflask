b85585de5d3a0af62f3576dfb85fccf5e16a5a90
GuilhermeVinicius01 <guilhermevinicius.d.s@gmail.com>
2026-08-24 20:44:07 -0300

    ....

--- app.py
+++ app.py
@@ -6,15 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template ('index.html')
+    return render_template ('dashboard/index.html')
 
 @app.route('/sobre')
 def sobre_o_sistema():
-    return render_template ('sobre.html')
+    return render_template ('dashboard/sobre.html')
 
-@app.route('/ajuda')
-def ajuda():
-    return render_template ('ajuda.html')
+@app.route('/aluno')
+def lista_aluno():
+    return render_template ('aluno/lista.html')
+
+@app.route('/professor')
+def lista_professor():
+    return render_template ('professor/lista.html')
 
 
 if __name__ == '__main__':
