fa6134a27237a7354bda6fd9c4cf8114a50832ef
GuilhermeVinicius01 <guilhermevinicius.d.s@gmail.com>
2026-08-24 20:42:30 -0300

    .....

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Olá, Flask está funcionando!'
+    return render_template ('index.html')
+
+@app.route('/sobre')
+def sobre_o_sistema():
+    return render_template ('sobre.html')
+
+@app.route('/ajuda')
+def ajuda():
+    return render_template ('ajuda.html')
 
 
 if __name__ == '__main__':
