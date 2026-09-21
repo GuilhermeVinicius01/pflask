2b8e3dc9b16140e2b9f383abae376c3b834087d2
GuilhermeVinicius01 <guilhermevinicius.d.s@gmail.com>
2026-09-21 19:28:05 -0300

    ....

--- app.py
+++ app.py
@@ -17,46 +17,28 @@ turma_dao = TurmaDAO()
 def home():
     return render_template('dashboard/index.html')
 
-
 @app.route('/sobre')
 def sobre_o_sistema():
     return render_template('dashboard/sobre.html')
-
 
 @app.route('/contato')
 def contato_dev():
     return render_template('dashboard/contato.html')
 
-
 @app.route('/aluno')
 def listar_aluno():
     lista = aluno_dao.listar_alunos()
-
-    return render_template(
-        'aluno/lista.html',
-        lista=lista
-    )
-
+    return render_template('aluno/lista.html',lista=lista)
 
 @app.route('/professor')
 def listar_professor():
     lista = professor_dao.listar_professores()
-
-    return render_template(
-        'professor/lista.html',
-        lista=lista
-    )
-
+    return render_template('professor/lista.html',lista=lista)
 
 @app.route('/turma')
 def listar_turma():
     lista = turma_dao.listar_turmas()
-
-    return render_template(
-        'turma/lista.html',
-        lista=lista
-    )
-
+    return render_template('turma/lista.html',lista=lista)
 
 if __name__ == '__main__':
     app.run(debug=True)
